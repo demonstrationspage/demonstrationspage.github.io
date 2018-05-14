79b8c5d9fa732d585f57a0416b6fbed110459ce4
demonstrationspage <37229344+demonstrationspage@users.noreply.github.com>
2018-05-14 22:08:05 +0300

    add files

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function () {
+$('#categories a').click(function () {
+       $('#categories .active').removeClass('active');
+       $(this).closest("li").addClass('active');
+       return false;
+    });
+    $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+    scrollTop: $($anchor.attr('href')).offset().top
+    }, 1200, 'easeInOutExpo');
+    event.preventDefault();
+    });
+});
